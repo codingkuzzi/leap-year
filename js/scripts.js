5cf85b461a21e330ac1359bc7323346ee1b371cd
Anna Kuznetsova and Joe Kramer <joe@>
2017-06-07 08:50:40 -0700

    leap year webpage with basic business logic

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//FRONT END
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
+
+//BACK END
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
+    return true;
+  } else {
+    return false;
+  }
+};
